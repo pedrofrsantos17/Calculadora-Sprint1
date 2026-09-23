3c421c463b79990e4ef15e5ed9b8bd2b39d1770c
pedrofrsantos17 <pedro.rsantos@sptech.school>
2026-09-23 19:53:57 -0300

    feat: adiciona API

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,10 +19,10 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
+            host: '127.0.0.1',
+            user: 'root',
+            password: ' Stark110521*',
+            database: 'TesteAPI',
             port: 3306
         }
     ).promise();
@@ -51,8 +51,8 @@ const serial = async (
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
         const valores = data.split(';');
-        const sensorDigital = parseInt(valores[1]);
-        const sensorAnalogico = parseFloat(valores[0]);
+        const sensorDigital = parseInt(valores[0]);
+        const sensorAnalogico = parseFloat(valores[1]);
 
         // armazena os valores dos sensores nos arrays correspondentes
         valoresSensorAnalogico.push(sensorAnalogico);
